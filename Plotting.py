481a575c9305eda09d103309c6f79b6cba9446b5
TobyNoneKanoby <131455513+TobyNoneKanoby@users.noreply.github.com>
2023-04-25 11:16:19 +0200

    Update Plotting.py

--- Plotting.py
+++ Plotting.py
@@ -1,48 +1,69 @@
-import csv
 import matplotlib.pyplot as plt
-from time import sleep
-from os import system
-import sys
-import time
-
-start_time=time.time()
+import csv
+import matplotlib.dates as mdates
+from datetime import datetime
 
 
-# Open the CSV file and read the data into lists
-with open('fuktighet.csv', 'r') as csvfile:
-    reader = csv.DictReader(csvfile)
-    timestamps = []
-    temps = []
-    humids1 = []
-    humids2 = []
-    for row in reader:
-        timestamps.append(row['time'])
-        #temps.append(float(row['temperature sensor 1']))
-        humids1.append(float(row['humidity sensor 1']))
-        humids2.append(float(row['humidity sensor 2']))
 
-# Create the plot
-fig, ax = plt.subplots(num='plot')
+# Define the file name
+filename = '/home/tobias/fuktighet_V2.csv'
+
+# Define the lists to store the data
+timestamps = []
+humidities1 = []
+humidities2 = []
+vanning_ack1 = []
+vanning_ack2 = []
+
+# Read the data from the CSV file
+with open(filename, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    next(csvreader) # Skip the header row
+    for row in csvreader:
+        timestamps.append(row[0])
+        humidities1.append(int(row[2]))
+        humidities2.append(int(row[3]))
+        vanning_ack1.append(int(row[4]) if row[4] else None) # Convert empty string to None
+        vanning_ack2.append(int(row[5]) if row[5] else None) # Convert empty string to None
+
+# Convert timestamps to datetime objects
+timestamps = [datetime.strptime(ts, '%m-%d %H:%M') for ts in timestamps]
+
+# Create the scatter plot
+plt.scatter(timestamps, humidities1, label='Plante 1')
+plt.scatter(timestamps, humidities2, label='Plante 2')
+
+for i, vanning1 in enumerate(vanning_ack1):
+    if vanning1 == 1:
+        plt.scatter(timestamps[i], humidities1[i], marker='x', color='blue', s=200)
+
+for i, vanning2 in enumerate(vanning_ack2):
+    if vanning2 == 1:
+        plt.scatter(timestamps[i], humidities2[i], marker='x', color='orange', s=200)
 
 
+plt.plot(timestamps, humidities1, color='blue', alpha=0.5)
+plt.plot(timestamps, humidities2, color='orange', alpha=0.5)
+
+# Set the x-tick labels to show only one tick per day
+days = mdates.DayLocator()
+date_fmt = mdates.DateFormatter('%d-%m')
+plt.gca().xaxis.set_major_locator(days)
+plt.gca().xaxis.set_major_formatter(date_fmt)
+#plt.xticks(rotation=45)
 
 manager = plt.get_current_fig_manager() #fullskjerm
 manager.full_screen_toggle()
-
-#ax.plot(timestamps, temps, label='Temperature')
-ax.plot(timestamps, humids1, label='Plante 1')
-ax.plot(timestamps, humids2, label='Plante 2')
-ax.set_xlabel('Tid')
-ax.set_ylabel('Prosent fuktighet')
-ax.set_title('Fuktighet')
-plt.ylim(50, 80)
-#plt.xticks(timestamps, [k for k in range(0,len(timestamps),5)])
-#plt.grid(color='r', linestyle='-', linewidth=1)
-ax.legend()
-
-# Show the plot
+plt.grid(True)
+plt.ylim(50, 90)
+plt.xlabel('Tid [ved midnatt]')
+plt.ylabel('Fuktighet [%]')
+plt.title('Fuktighetsavlesning')
+plt.legend()
 plt.show(block=False)
 plt.pause(20)#viser i 20 sek før den lukker
 plt.close()
+
+
 
 
